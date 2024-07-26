3819edd757d5dadb333b0f65b9e10b36b0afecc4
faris <fariskp46820@gmail.com>
2024-07-26 18:11:20 +0530

    New_project_with_Razor page added and basic setup and category list done

---
+++ Bulky/BulkeyRazor_Temp/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkeyRazor_Temp.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Display Order Must Be Between 1-100")]
+        [DisplayName("Display Order")]
+        public int DiplayOrder { get; set; }
+    }
+}
