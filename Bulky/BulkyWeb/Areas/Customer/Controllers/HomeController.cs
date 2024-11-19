b08a78df212e161d7669f7b55e986a1d09522cc7
faris <fariskp46820@gmail.com>
2024-11-19 20:22:32 +0530

    SessionBug was fixed and also add log out session for cart clear

--- Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if(claim!= null)
+            {
+               HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+            }
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includePropeties:"Category");
             return View(productList);
         }
